a203303fb234ddfabe651363518c203d7a9aa2b3
Benjamin Ramas <benjaminramas119@gmail.com>
2025-05-02 15:24:54 +0100

    Fixed getspecificcountry routes

--- src/app/api/getspecificcountry/[id]/route.ts
+++ src/app/api/getspecificcountry/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
+import { Context } from "vm";
 
-export async function GET( request: Request, context: any){
+export async function GET(context: Context){
     const data = await fetch(`https://restcountries.com/v3.1/name/${context.params.id}?fullText=true`);
     const posts: object = await data.json();
     return NextResponse.json({posts});
