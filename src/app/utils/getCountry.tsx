5dda84e810f7c5db24c0c6b138f794397011bf75
Benjamin Ramas <benjaminramas119@gmail.com>
2025-05-02 17:50:54 +0100

    Updated to fit with ESLint rules

--- src/app/utils/getCountry.tsx
+++ src/app/utils/getCountry.tsx
@@ -21,11 +21,6 @@ export type fetchedCountryData = {
 const GetCountries = async(setData: (d: any) => void) => {
     const data = await fetch('/api/getcountries');
     const posts = await data.json();
-    const sorted = posts.sort(function(a: fetchedCountryData, b: fetchedCountryData) {
-        var textA = a.name.common.toUpperCase();
-        var textB = b.name.common.toUpperCase();
-        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    });
     return setData(posts);
 }
 
