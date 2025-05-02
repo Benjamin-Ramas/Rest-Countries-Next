58ce1024ff5c19d355c841a9db3bda255133653f
Benjamin Ramas <benjaminramas119@gmail.com>
2025-05-02 15:23:57 +0100

    Got rid of unused requests on api routes

--- src/app/api/getcountrycode/[id]/route.ts
+++ src/app/api/getcountrycode/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { Context } from "vm";
 
-export async function GET(request: Request, context: Context){
+export async function GET(context: Context){
     console.log(`Contexts: ${context.params.id}`)
     const res = await fetch(`https://restcountries.com/v3.1/alpha/${context.params.id}`);
     const posts = await res.json();
--- src/app/api/getlang/[id]/route.ts
+++ src/app/api/getlang/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { Context } from "vm";
 
-export async function GET(request: Request, context: Context){
+export async function GET(context: Context){
     const n = context.params.id;
     const res = await fetch(`https://restcountries.com/v3.1/lang/${n}`);
     const posts = await res.json();
