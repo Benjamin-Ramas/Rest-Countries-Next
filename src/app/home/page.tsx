5feaf0b5fc93175c5068a0fced5ddc755a3d6a38
Benjamin Ramas <benjaminramas119@gmail.com>
2025-05-02 15:26:28 +0100

    Got rid of unused response variables

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,24 +5,11 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import Loading from "./loading";
 
-const fetchApiData = async () => {
-    try{
-        const response = await fetch('/api/get_specific_country/belgium')
-        .then((response) => response.json())
-        .then((response) => {
-            console.log(response.posts[0]);
-        })
-    } catch(err){
-        console.error(err);
-    }
-}
-
 export default function Page(){
     const {theme} = useTheme();
     const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetchApiData();
         setLoading(false);
     }, []);
 
